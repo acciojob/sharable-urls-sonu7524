320d276aaa1011106c6c625e29ac2f1aef800bb4
Sonu Daryani <100096513+sonu7524@users.noreply.github.com>
2023-05-08 20:57:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-// your code here
+const button = document.getElementById("button");
+const url = document.getElementById("url");
+const baseURL = "https://localhost:8080/";
+
+button.addEventListener("click", function(event) {
+	event.preventDefault();
+    url.textContent = baseURL;
+    let name = document.getElementById("name").value;
+    let year = document.getElementById("year").value;
+    let query = `?name=${name}&year=${year}`;
+    if(name === "" && year !== ""){
+        query = `?year=${year}`;
+    }
+    if(name !== "" && year === ""){
+        query = `?name=${name}`;
+    }
+    if(name === "" && year === ""){
+        query = ``;
+    }
+    let newUrl = url.textContent + query;
+	url.textContent = newUrl;
+});
